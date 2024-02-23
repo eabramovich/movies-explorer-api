355d2c0775f904d78878f00b60cc4fce54a768b7
Evgeniya Abramovich <ewg.abramovich2014@yandex.ru>
2024-02-23 19:23:16 +0300

    Change port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import errorHandler from './middlewares/errorHandler.js';
 import { requestLogger, errorLogger } from './middlewares/logger.js';
 import limiter from './middlewares/rateLimiter.js';
 
-const { PORT = 3000, CONNECT_BD = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, CONNECT_BD = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 mongoose.connect(CONNECT_BD);
